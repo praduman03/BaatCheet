8fdafc0e378d8cea1ea1c6e2e9668f14fe2e60a5
praduman <praduman03k@gmail.com>
2024-04-06 15:27:08 +0530

    test clg 2

--- server/utils/generateToken.utils.js
+++ server/utils/generateToken.utils.js
@@ -8,7 +8,7 @@ const generateTokenandSetCookie = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 10 * 24 * 60 * 60 * 1000, // in ms
     httpOnly: true, //to prevent XSS attacks cross site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    sameSite: "lax", // CSRF attacks cross-site request forgery attacks
     secure: process.env.NODE_ENV !== "development" ? true : false
   });
 };
